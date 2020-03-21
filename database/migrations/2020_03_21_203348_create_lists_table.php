3d3b23f84df7850045c97806ea1912123e80bbf4
Andres Alvarez Skinner <alvarezskinner@gmail.com>
2020-03-21 20:56:12 +0000

    Creation of Migrations and associated Model

---
+++ database/migrations/2020_03_21_203348_create_lists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index();
+            $table->unsignedBigInteger('userId');
+            $table->timestamps();
+            $table->unique('name', 'userId');
+            $table->foreign('userId')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lists');
+    }
+}
