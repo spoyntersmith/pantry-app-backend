5b790b3de23aa211496e0fae15a85de82880fe81
spoyntersmith <spoyntersmith@users.noreply.github.com>
2020-03-22 14:31:31 +0000

    add items function for category

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * Get items in the category
+     */
+    $this->public function items()
+    {
+        return $this->hasMany('App\Items', 'foreign_key');
+    }
 }
