c07fb1a3ed9ed033073befe11ced59d02bdfd58b
spoyntersmith <spoyntersmith@users.noreply.github.com>
2020-03-22 14:16:26 +0000

    add get user and items functions

--- app/List.php
+++ app/List.php
@@ -4,7 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class List extends Model
+class List extends Model 
 {
-    //
+    /**
+     * Get the List's User
+     */
+    public function user()
+    {
+        return $this->hasOne('App\User', 'foreign_key', 'local_key');
+    }
+
+    /**
+     * Get the items in the List
+     */
+    public function items() 
+    {
+        return $this->hasMany('App\Items');
+    }
 }
