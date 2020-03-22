de4e3fa8f00c1ddf63e9bc07fcb2a32304c8abae
spoyntersmith <spoyntersmith@users.noreply.github.com>
2020-03-22 14:29:19 +0000

    add getter for the item's owner (list)

--- app/Item.php
+++ app/Item.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    /**
+     * Get the List that owns the Item
+     */
+    public function list()
+    {
+        return $this->belongsTo('App\List', 'foreign_key');
+    }
 }
