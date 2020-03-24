708084636ca965307c513a4c8a1d8d2f361aea96
spoyntersmith <spoyntersmith@users.noreply.github.com>
2020-03-24 23:37:33 +0000

    generate faker data

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -3,10 +3,18 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Item;
+use App\Category;
+use App\ItemList;
 use Faker\Generator as Faker;
 
 $factory->define(Item::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->unique()->words(random_int(1, 2), true),
+        'expiryDate' => date_format($faker->dateTimeBetween('now', '+2 years'), 'd-m-Y'),
+        'bestBeforeDate' => date_format($faker->dateTimeBetween('now', '+2 years'), 'd-m-Y'),
+        'quantity' => $faker->numberBetween(1, 100),
+        'weight' => $faker->randomFloat(2, 0.0, 100),
+        'item_list_id' => ItemList::all()->random()->id,
+        'category_id' => Category::all()->random()->id,
     ];
 });
