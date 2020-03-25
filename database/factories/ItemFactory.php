437f03c7df898aef67371538241c82c9fdc87859
spoyntersmith <spoyntersmith@users.noreply.github.com>
2020-03-25 12:05:41 +0000

    remove date formatting on database

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -10,8 +10,8 @@ use Faker\Generator as Faker;
 $factory->define(Item::class, function (Faker $faker) {
     return [
         'name' => $faker->unique()->words(random_int(1, 2), true),
-        'expiryDate' => date_format($faker->dateTimeBetween('now', '+2 years'), 'd-m-Y'),
-        'bestBeforeDate' => date_format($faker->dateTimeBetween('now', '+2 years'), 'd-m-Y'),
+        'expiryDate' => $faker->dateTimeBetween('now', '+2 years'),
+        'bestBeforeDate' => $faker->dateTimeBetween('now', '+2 years'),
         'quantity' => $faker->numberBetween(1, 100),
         'weight' => $faker->randomFloat(2, 0.0, 100),
         'item_list_id' => ItemList::all()->random()->id,
